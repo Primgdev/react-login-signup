a09c6469a553d726a065a28acd7a2fbba34af738
Primgdev <primgdev@gmail.com>
2023-10-04 20:22:02 +1100

    frontend update

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,20 +1,22 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function login() {
   return (
-    <div>
-      <div>
+    <div className='d-flex justify-content-center align-items-center bg-pimary'> 
+      <div className='p-3 bg-white w-25'>
         <form action=''>
-            <div>
+            <div className='mb-3'>
                 <label htmlFor='email'>Email</label>
-                <input type='email' placeholder='Enter Email' />
+                <input type='email' placeholder='Enter Email'  className='form-control'/>
 
             </div>
             <div className='mb-3'>
               <label htmlFor='password'> password</label>
-              <input type='password' placeholder='Enter Password' />
+              <input type='password' placeholder='Enter Password' className='form-control'/> 
               
                </div> 
+               <button className='btn btn-success'>Login</button>
         </form>
       </div>
     </div>
